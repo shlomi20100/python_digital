15f192d9b801187c8cff482288683703216639da
shlomi20100 <shlomi20100@gmail.com>
2021-02-02 21:11:07 +0200

    holla y'all that's my first exercise for now on python 3.7

---
+++ Lesson 1/targil 1.py
@@ -0,0 +1,5 @@
+name=input("Enter your name: ")
+age=input("Enter your age: ")
+mail=input("Enter your mail: ")
+
+print("\nFull name: " + name + "\nYour age is:  " + str(age) + "\nYour mail: " + mail )
