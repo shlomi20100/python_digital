0273f6d496dddafcb75c446a18eafa3cf49da399
shlomi20100 <shlomi20100@gmail.com>
2021-02-11 20:40:55 +0200

    that's my 8th Lab exercise for now on python 3.7

--- python learning/LAB 8.py
+++ python learning/LAB 8.py
@@ -8,16 +8,16 @@ from time import sleep
 from random import randint
 
 while(True):
-    choice=input("Menu: \n------------------\n1.printing 100 numbers\n2.checking fibo\n3.randit numbers until we get 12 or we count 10 times\n")
+    choice=input("Menu: \n------------------\n1 .printing 100 numbers\n2 .checking fibo\n3 .randit numbers until we get 12 or we count 10 times\n")
 #1
-    if(choice==1):
+    if(choice=="1"):
         for i in range(1,101):
             print(i)
 #2
-    elif(choice==2):
-#fibo=[1, 2, 3, 5, 8, 13, 21]
-       fibo=[]
-        for i in range(7)
+    elif(choice=="2"):
+        #fibo=[1, 2, 3, 5, 8, 13, 21]
+        fibo=[]
+        for i in range(7):
             fibo.append(int(input("Enter a number: ")))
         boolean = "True"
         for i in range(2, len(fibo)):
@@ -33,17 +33,17 @@ while(True):
             else:
                 print("It isn't fibo series")
 #3
-    elif(choice==3):
+    elif(choice=="3"):
         counter = 1
-        num=randit(1,12)
+        num=randint(1,12)
         while(num!=12 and counter<11):
             print("Counter" + str(counter) + " Number: " + str(num) + "\n")
             counter=counter+1
-            num = randit(1,12)
+            num=randint(1,12)
         else:
             print("Enter 1-3 only please!\n")
             continue
-        exit=input("Do you want to exit? yes/no?\n")
+        exit=input("Do you want to exit? yes/no\n")
         if(exit=="yes"):
             print("Thank you and have a good day!\n")
             break
